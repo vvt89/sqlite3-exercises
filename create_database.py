97f35c2f44f1c19ef0299c1311e3a6cead29645d
Uladzimir Vishniareuski <vishnerevsky@gmail.com>
2019-06-29 14:29:49 +0300

    Third Commit. Several comments have been added

--- create_database.py
+++ create_database.py
@@ -1,6 +1,7 @@
 import sqlite3
 import random
 
+# Список имен пользователей интернет-магазина
 user_list = [['Svetlana', 'Ivanova'],
              ['Olga', 'Petrova'],
              ['Nikolay', 'Alexandrov'],
@@ -9,6 +10,7 @@ user_list = [['Svetlana', 'Ivanova'],
              ['Violetta', 'Durkina'],
              ['Stepan', 'Pichonin']]
 
+# Список товаров
 items_list = [['sclissors', 20, 'simple sclissors', 10],
               ['paper', 2, 'simple paper', 9],
               ['knife', 15, 'simple knife', 8],
@@ -18,10 +20,11 @@ items_list = [['sclissors', 20, 'simple sclissors', 10],
               ['butter', 34, 'simple butter', 10],
               ['egg', 12, 'simple egg', 9]]
 
-
+# Подключаемся к файлу базы данных и создаем курсор
 conn = sqlite3.connect('shop.sqlite')
 cur = conn.cursor()
 
+# Создаем таблицы в базе данных, если они существуют, то удаляем
 cur.execute('DROP TABLE IF EXISTS Sess')
 cur.execute('CREATE TABLE Sess (id INTEGER, user_id INTEGER, sess TEXT)')
 cur.execute('DROP TABLE IF EXISTS User')
@@ -35,17 +38,19 @@ cur.execute('CREATE TABLE Basket (id INTEGER, user_id INTEGER)')
 cur.execute('DROP TABLE IF EXISTS BasketItems')
 cur.execute('CREATE TABLE BasketItems (id INTEGER, basket_id INTEGER, item_id INTEGER)')
 
-
+# Заполняем таблицу с именами пользователей
 for n,i in enumerate(user_list):
     params = (n, i[1]+str(n*n+1980), i[0], i[1], "123456")
     cur.execute('INSERT INTO User (id, username, first_name, last_name, pass) VALUES(?,?,?,?,?)', params)
 conn.commit()
 
+# Заполняем таблицу с наименованиями товаров
 for n,i in enumerate(items_list):
     params = (n, i[0], i[1], i[2], i[3])
     cur.execute('INSERT INTO Items (id, name, price, description, rating) VALUES(?,?,?,?,?)', params)
 conn.commit()
 
+# Присваиваем пользователям по одной корзине
 basket_ids = list(range(len(user_list)))
 used_basket_ids = []
 for i in range(len(user_list)-1):
@@ -56,6 +61,7 @@ for i in range(len(user_list)-1):
     used_basket_ids.append(rand_id)
 conn.commit()
 
+# Наполняем корзины покупателей товарами
 probability_list = [0, 1, 2, 3]
 item_ids = list(range(len(items_list)))
 for i in range(len(user_list)):
@@ -70,6 +76,7 @@ for i in range(len(user_list)):
         cur.execute('INSERT INTO BasketItems (id, basket_id, item_id) VALUES(?,?,?)', params)
 conn.commit()
 
+# Вывод полученных таблиц в консоль:
 print("Users:")
 cur.execute('SELECT * FROM User')
 for row in cur:
@@ -87,4 +94,5 @@ cur.execute('SELECT * FROM BasketItems')
 for row in cur:
     print(row)
 
+#
 cur.close()
